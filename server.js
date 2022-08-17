59c6b1a15a13c1bf3fa83aa84b066b4c041ff36e
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2022-08-17 11:13:49 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const WebSocketServer = require("ws").Server;
+const path = require("path");
+const port = process.env.PORT || 8010;
+
+// GET private IP (for development purposes)
+require("dns").lookup(require("os").hostname(), function (err, add, fam) {
+  console.log(add + ":" + port);
+});
+
+const server = app.listen(port, () => {
+  if (process.send) {
+    process.send(`Server running on port ${port}\n\n`);
+  }
+});
+
+app.use("/shell", express.static(path.join(__dirname, "shell/")));
+
+// Kahoot server
+
+// We then create a new variable which will store the actual server I'll be running
+const ws = new WebSocketServer({
+  // Then we set the parameter of httpServer to the server variable that we said that would be listening on the port specified
+  //httpServer: server -- enable if using HTTP server
+  noServer: true,
+});
+
+server.on("upgrade", (request, socket, head) => {
+  ws.handleUpgrade(request, socket, head, (websocket) => {
+    ws.emit("connection", websocket, request);
+  });
+});
+
+ws.on("connection", (websocketConnection) => {
+  console.log("[CONNECTION] Client is Attempting To Connect!");
+
+//   websocketConnection.send();
+
+  websocketConnection.on("message", (message) => {
+    let data;
+    // Check is isJSON
+    try {
+      data = JSON.parse(message);
+    } catch (error) {
+      console.warn("[SUBSYSTEM] Format Unsupported");
+      return;
+    }
+
+  });
+});
+
+
+
+ws.on("close", () => {
+  console.log("[CONNECTION] Client has disconnected.");
+  systemBroadcast("Client has disconnected from the chat.");
+});
+
+ws.broadcast = (data) => {
+  ws.clients.forEach((client) => client.send(data));
+};
+
+
+// Send a heartbeat to the client every 20s
+// We do this to prevent the socket from commiting suicide.
+setInterval(() => {
+  ws.broadcast(JSON.stringify(chatlog));
+}, 20000);
