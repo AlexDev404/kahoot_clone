43b922a3e64971aa85bb5f61d322c2a695c329bf
AlexDev404 <alexdev404@users.noreply.github.com>
2022-08-19 19:41:04 -0600

    Mobility + Sample Questions Fix

--- system/rooms/rooms.data.global.js
+++ system/rooms/rooms.data.global.js
@@ -22,8 +22,8 @@ exports.default = {
           30,
         ],
         [
-          ("The wampa on Hoth fed on a tauntaun. On Earth, a meat-eater is called",
-          30),
+          "The wampa on Hoth fed on a tauntaun. On Earth, a meat-eater is called",
+          30,
         ],
         [
           'The Star Wars galaxy contains many planets. What defines a "planet" in our galaxy?',
